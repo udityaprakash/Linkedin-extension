bdebdc8f699deb514646c887033cf7a958c01e2b
udityaprakash <udityaprakash983@gmail.com>
2024-05-07 20:05:24 +0530

    Salutation added in script

--- add.js
+++ add.js
@@ -27,12 +27,28 @@
               setTimeout(() => {
                 var toInput = document.querySelector(".msg-form__contenteditable");
                 if (toInput) {
-                  toInput.innerHTML = "<p>hello sir i am a great fan of yours<p>";
+                  var gen = document.querySelectorAll(".mt2 div div span")[1].innerHTML;
+                  const genderRegex = /\(([^)]+)\)/;
+                  const match = gen.match(genderRegex);
+                  const gend = match ? match[1] : '';
+                  const salutation = genderName(gend); 
+
+                  toInput.innerHTML = "<p>hello "+salutation+" ,I am a great fan of yours<p>";
                 }
               }, 1000);
               // document.querySelectorAll(".msg-form__contenteditable")[0].innerHTML=`<p>hello sir i am a great fan of yours<p>`;
             }
           });
+        }
+      }
+      
+      function genderName(str){
+        switch(str){
+          case "She/Her":
+            return "Ma'am";
+          case "He/Him":
+            return "Sir";
+          default: return "";    
         }
       }
     
