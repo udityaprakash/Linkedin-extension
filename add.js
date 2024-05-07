acfd91b493062e0a031897814901617352e818cf
udityaprakash <udityaprakash983@gmail.com>
2024-05-07 19:51:29 +0530

    updated code to open the msg box and type the msg

--- add.js
+++ add.js
@@ -9,7 +9,8 @@
         a.innerHTML = "Get Referred";
         // document.querySelector(".SndfhJCRCNhMRfeLPAudhPMGuRexFAUnCgtCQ.mt2").appendChild(sp);
         // document.querySelector("#ref-span").appendChild(a);
-        var target = document.querySelector(".debWPjvoLthHmxanBpRdaFCxfxOIAXtjuGI.mt2");
+        // var target = document.querySelector(".debWPjvoLthHmxanBpRdaFCxfxOIAXtjuGI.mt2");
+        var target = document.querySelectorAll(".mt2")[0];
         if (target) {
         target.appendChild(sp);
         document.querySelector("#ref-span").appendChild(a);
@@ -23,6 +24,13 @@
             var composeButton = document.querySelector(".entry-point.profile-action-compose-option button");
             if (composeButton) {
               composeButton.click();
+              setTimeout(() => {
+                var toInput = document.querySelector(".msg-form__contenteditable");
+                if (toInput) {
+                  toInput.innerHTML = "<p>hello sir i am a great fan of yours<p>";
+                }
+              }, 1000);
+              // document.querySelectorAll(".msg-form__contenteditable")[0].innerHTML=`<p>hello sir i am a great fan of yours<p>`;
             }
           });
         }
