620471bbe7a0bd681526cbac258d80fa6193f8fd
udityaprakash <udityaprakash983@gmail.com>
2024-05-13 18:06:29 +0530

    msg formatted and refined

--- add.js
+++ add.js
@@ -26,22 +26,13 @@
                   const name = (document.querySelectorAll(".mt2 div div span a h1")[0].innerHTML).split(" ")[0];
                   const education = document.querySelectorAll(".mr1 span");
                   toInput.innerHTML = `<p></p>`;
-                  const msg = `<p>Hello ${name},</p>
-                  <p><br></p>
-                  <p>I hope this message finds you well. I am reaching out to you with a request
-                  for a referral in your current company.
-                  </p>
-                  <p><br></p>
-                  <p>I'm Uditya Prakash, a Backend Developer with 3 years of project experience in NodeJs and its subsequent frameworks. I'm interested in joining your company and would appreciate any insights you might have about the work culture and any upcoming SDE roles.
-                  </p>
-                  <p><br></p>
-                  <p>
-                  I have attached my resume for your reference. Looking forward to hearing
-                  from you.
-                  </p>
-                  <p><br></p>
+                  const msg = `<p>Hello ${name},</p><br>
+                  <p>I hope this message finds you well. I am reaching out to you with a request for a referral in your current company.</p><br>
+                  <p>I'm Uditya Prakash, a Backend Developer with 3 years of project experience in NodeJs and its subsequent frameworks. I'm interested in joining your company and would appreciate any insights you might have about the work culture and any upcoming SDE roles.</p><br>
+                  <p>I have attached my resume for your reference. Looking forward to hearing from you.</p><br>
                   <p>Best Regards,</p>
                   <p>Uditya Prakash</p>`;
+                  toInput.setAttribute("focused", "true");
                   toInput.innerHTML = msg;
                 }
               }, 1000);
